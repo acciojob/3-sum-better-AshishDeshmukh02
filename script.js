a05a2d47609ed37d699344b932e2872120390ed6
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-17 14:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function threeSum(S, target) {
+  S.sort(function(a, b) {
+    return a - b;
+  });
+  var closestSum = S[0] + S[1] + S[2];
+
+  for (var i = 0; i < S.length - 2; i++) {
+    var j = i + 1;
+    var k = S.length - 1;
+    while (j < k) {
+      var currentSum = S[i] + S[j] + S[k];
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+      if (currentSum < target) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+  return closestSum;
 }
 
 module.exports = threeSum;
